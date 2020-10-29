459eadece4696e50be819a8617216503b3fec7af
Agustin <agu@cursosit.com>
2020-10-29 15:51:37 -0300

    add input and state hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['hola', 'hey']);
+
+  console.log(input);
+  console.log(messages);
+
+
+  const sendMessage = (event) => {
+    // all the logic to send a message goes 
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello!!</h1>
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <button>Send message</button>
+      {/* input field */}
+      {/* button */}
+      {/* messages themselves */}
     </div>
   );
 }
